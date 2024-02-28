e5dfc18c25fec5955ba8c280c38645fcfbacfdb5
krisna <saonoke21@gmail.com>
2024-02-28 14:56:26 +0700

    feat: Controller

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    //
+    public function index()
+    {
+        return 'Nama: Krisna Andika Wijaya, NIM : 2241720118';
+    }
+}
